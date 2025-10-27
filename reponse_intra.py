338b5870252b75eb026534b205df2fe82dd86096
6295095 <6295095@etu.cegepsth.qc.ca>
2025-10-27 11:55:40 -0400

    intra 1

--- reponse_intra.py
+++ reponse_intra.py
@@ -1,3 +1,18 @@
 ##########################################
-# Nom, Prénom, <Numéro de DA>
+# Labrecque Marotte,Jude<6295095>
 ##########################################
+import random
+
+temperature = random.uniform(20,35)
+print(temperature)
+
+
+def temperature():
+
+
+    if temperature > 30:
+        print("Trop chaud")
+    if temperature < 24:
+        print("Trop froid")
+    else:
+        print("OK")
